56dfeee2d3288ed9550dd3eef5e8486e9e3cb35a
João Gabriel <joaogabrielmonteg41@gmail.com>
2025-11-11 09:26:24 -0300

    Corrigido .env

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://family-finance-app-two.vercel.app', // ✅ URL pública do backend na Vercel
+  baseURL: process.env.REACT_APP_API_URL,
 });
 
 api.interceptors.request.use((config) => {
